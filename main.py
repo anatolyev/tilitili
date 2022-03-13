c17b58ae4864eee18372721a32f033e14e53b9b4
Алексей Анатольев <73399971+anatolyev@users.noreply.github.com>
2022-03-13 22:01:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 from flask import Flask
-from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)
 
 @app.route("/")
 def index():
     return "Привет от приложения Алексея"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
