ce4878c60f6a5302995c068d303c01c64b24ed9e
Алексей Анатольев <73399971+anatolyev@users.noreply.github.com>
2022-03-13 21:55:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ run_with_ngrok(app)
 def index():
     return "Привет от приложения Алексея"
 
-
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
